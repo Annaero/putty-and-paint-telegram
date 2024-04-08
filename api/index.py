3f4152c6d30c8eb822544e9fcdb6905b104cc12a
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2024-04-09 02:04:42 +0400

    publish in channel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,20 @@
 from http.server import BaseHTTPRequestHandler
+import asyncio
 from bs4 import BeautifulSoup
 import requests
 import os
+from telegram import Bot
+from urllib.parse import urljoin
 
 ROOT_URL = os.environ["PUTTY_AND_PAINT"]
-PROJECTS_URL = ROOT_URL + os.environ["PROJECTS_URL"]
+PROJECTS_URL = urljoin(ROOT_URL, os.environ["PROJECTS_URL"])
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+print(BOT_TOKEN)
+
+
+async def send_photo(photo, msg):
+    await Bot(BOT_TOKEN).send_photo(chat_id=CHAT_ID, photo=photo, caption=msg)
 
 
 class handler(BaseHTTPRequestHandler):
@@ -28,11 +38,18 @@ class handler(BaseHTTPRequestHandler):
         for project_link in project_links:
             if "href" not in project_link.attrs:
                 print("WARNING:", f"One of project does not have href {project_link} ")
+            else:
+                href = project_link.attrs["href"]
+                title = project_link.attrs["title"]
+                caption = "\n".join([title, href])
             print(project_link.attrs["href"])
             try:
                 image = project_link.find("img")
-                print(image.attrs["src"])
+                image_path = image.attrs["src"]
             except Exception as e:
                 print("WARNING:", f"Failed get image for {project_link} with error {e}")
+
+        image_url = urljoin(ROOT_URL, image_path)
+        asyncio.run(send_photo(image_url, caption))
 
         return
