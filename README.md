99a2745191de2effa48557978b0ea52563ce734d
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2024-04-09 01:17:03 +0400

    scrap p&p page

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python
+```shell
+    pip install pipenv
+    pipenv shell
+    pipenv install
+```
+
+# Vercel
+```shell
+    npm i -g vercel
+    vercel dev
+```
--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-import logging
+from bs4 import BeautifulSoup
+import requests
 import os
+
+ROOT_URL = os.environ["PUTTY_AND_PAINT"]
+PROJECTS_URL = ROOT_URL + os.environ["PROJECTS_URL"]
 
 
 class handler(BaseHTTPRequestHandler):
@@ -8,7 +12,27 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
+        self.wfile.write("Hello, world!".encode("utf-8"))
 
-        logging.log("Enter handler")
+        print(
+            "INFO:", f"Requesting projects page {PROJECTS_URL}"
+        )  # By some reason `logging` do not works correctly with Vercel
+        response = requests.get(PROJECTS_URL)
+
+        if response.status_code != 200:
+            print("ERROR:", f"Got response: {response}")
+            return
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        project_links = soup.find_all("a", class_="project-link")
+        for project_link in project_links:
+            if "href" not in project_link.attrs:
+                print("WARNING:", f"One of project does not have href {project_link} ")
+            print(project_link.attrs["href"])
+            try:
+                image = project_link.find("img")
+                print(image.attrs["src"])
+            except Exception as e:
+                print("WARNING:", f"Failed get image for {project_link} with error {e}")
 
         return
