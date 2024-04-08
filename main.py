1c3c0aaba75e8ac838984f260c8a0ac0ab250aef
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2024-04-08 23:46:52 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+import logging
+import os
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        TG = os.environ["tg"]
+
+        return
